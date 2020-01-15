f6154eb1544f2935143cf64e8b0eac3c14d50853
zuqq <noahheld17@gmail.com>
2020-01-15 14:07:28 +0100

    100% coverage for bencoding

--- tests/test_bencoding.py
+++ tests/test_bencoding.py
@@ -1,4 +1,6 @@
-from wave import bencoding
+import pytest
+
+from surge import bencoding
 
 
 def test_decode():
@@ -12,3 +14,10 @@ def test_decode():
         b"spam": b"eggs",
     }
     assert bencoding.decode(b"d4:spaml1:a1:bee") == {b"spam": [b"a", b"b"]}
+
+def test_raw_val():
+    assert bencoding.raw_val(b"d3:cow3:moo4:spam4:eggse", b"cow") == b"3:moo"
+    assert bencoding.raw_val(b"d3:cow3:moo4:spam4:eggse", b"spam") == b"4:eggs"
+    assert bencoding.raw_val(b"d4:spaml1:a1:bee", b"spam") == b"l1:a1:be"
+    with pytest.raises(KeyError):
+        bencoding.raw_val(b"d4:spaml1:a1:bee", b"eggs")
