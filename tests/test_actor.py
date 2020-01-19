9740d52dfc59cc31133845904499676e5e8d23b2
zuqq <noahheld17@gmail.com>
2020-01-19 20:43:58 +0100

    Clean up actor tests

--- tests/test_actor.py
+++ tests/test_actor.py
@@ -11,6 +11,7 @@ async def test_start():
     a = actor.Actor()
 
     await a.start()
+
     assert a._running
 
 
@@ -19,9 +20,8 @@ async def test_start_stop():
     a = actor.Actor()
 
     await a.start()
-    assert a._running
-
     await a.stop()
+
     assert not a._running
     assert a._crashed
 
@@ -30,10 +30,11 @@ async def test_start_stop():
 async def test_stop_children():
     parent = actor.Actor()
     child = unittest.mock.Mock(spec_set=actor.Actor)
+
     await parent.start()
     await parent.spawn_child(child)
-
     await parent.stop()
+
     child.stop.assert_awaited()
 
 
@@ -49,8 +50,8 @@ async def test_crash_reporting():
     child = actor.Actor()
     child._on_start = unittest.mock.AsyncMock(side_effect=RuntimeError)
     child._monitor_children = unittest.mock.AsyncMock()
-    await child.start(parent)
 
+    await child.start(parent)
     # Finish all scheduled tasks.
     tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
     await asyncio.gather(*tasks, return_exceptions=True)
@@ -66,6 +67,7 @@ async def test_spawn_child():
     type(child)._parent = unittest.mock.PropertyMock()
 
     await parent.spawn_child(child)
+
     assert child in parent._children
     child.start.assert_awaited()
     child._parent._assert_called_once_with(parent)
@@ -79,9 +81,9 @@ async def test_crashed_children_put():
 
     await parent.start()
     await parent.spawn_child(child)
-
     with unittest.mock.patch.object(parent._crashed_children, "put_nowait") as mock_put:
         parent.report_crash(child)
+
     mock_put.assert_called_once_with(child)
 
 
@@ -93,10 +95,8 @@ async def test_crashed_children_get():
 
     await parent.start()
     await parent.spawn_child(child)
-
     with unittest.mock.patch.object(parent._crashed_children, "get") as mock_get:
         mock_get.side_effect = (child,)
-
         # Finish all scheduled tasks.
         tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
         await asyncio.gather(*tasks, return_exceptions=True)
