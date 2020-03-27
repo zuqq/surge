b649a56e15d51843cc3ea433710e5cb879c42fee
zuqq <noahheld17@gmail.com>
2020-03-27 12:28:15 +0100

    Housekeeping

--- surge/bencoding.py
+++ surge/bencoding.py
@@ -1,64 +1,64 @@
 """Recursive descent parser for bencoding."""
 
 
-def _int(input, pos):
+def _int(bs, offset):
     # Note that this doesn't follow the specification in BEP 3, because it
     # ignores leading zeroes instead of throwing an error.
-    end = input.index(b"e", pos)
-    return end + 1, int(input[pos + 1 : end].decode("ascii"))
+    end = bs.index(b"e", offset)
+    return end + 1, int(bs[offset + 1 : end].decode("ascii"))
 
 
-def _list(input, pos):
+def _list(bs, offset):
     result = []
-    pos += 1
-    while input[pos] != ord("e"):
-        pos, rval = _parse(input, pos)
+    offset += 1
+    while bs[offset] != ord("e"):
+        offset, rval = _parse(bs, offset)
         result.append(rval)
-    return pos + 1, result
+    return offset + 1, result
 
 
-def _dict(input, pos):
+def _dict(bs, offset):
     result = {}
-    pos += 1
-    while input[pos] != ord("e"):
-        pos, key = _str(input, pos)
-        pos, result[key] = _parse(input, pos)
-    return pos + 1, result
+    offset += 1
+    while bs[offset] != ord("e"):
+        offset, key = _str(bs, offset)
+        offset, result[key] = _parse(bs, offset)
+    return offset + 1, result
 
 
-def _str(input, pos):
-    sep_index = input.index(b":", pos)
-    end = sep_index + int(input[pos:sep_index].decode("ascii")) + 1
-    return end, input[sep_index + 1 : end]
+def _str(bs, offset):
+    sep_index = bs.index(b":", offset)
+    end = sep_index + int(bs[offset:sep_index].decode("ascii")) + 1
+    return end, bs[sep_index + 1 : end]
 
 
-def _parse(input, pos):
-    if input[pos] == ord("i"):
-        return _int(input, pos)
-    if input[pos] == ord("l"):
-        return _list(input, pos)
-    if input[pos] == ord("d"):
-        return _dict(input, pos)
-    if input[pos] in (ord(str(i)) for i in range(10)):
-        return _str(input, pos)
+def _parse(bs, offset):
+    if bs[offset] == ord("i"):
+        return _int(bs, offset)
+    if bs[offset] == ord("l"):
+        return _list(bs, offset)
+    if bs[offset] == ord("d"):
+        return _dict(bs, offset)
+    if bs[offset] in (ord(str(i)) for i in range(10)):
+        return _str(bs, offset)
 
 
-def decode(input):
-    """Get the Python object corresponding to the bencoded object `input`."""
-    pos, rval = _parse(input, 0)
-    if pos == len(input):
+def decode(bs):
+    """Return the Python object corresponding to the bencoded object `bs`."""
+    offset, rval = _parse(bs, 0)
+    if offset == len(bs):
         return rval
     raise ValueError
 
 
-def raw_val(input, key):
-    """Get the bencoded value corresponding to `key` in the bencoded dictionary
-    `input`."""
-    pos = 1
-    while pos < len(input) and input[pos] != ord("e"):
-        pos, curr_key = _str(input, pos)
-        next_pos, _ = _parse(input, pos)
+def raw_val(bs, key):
+    """Return the bencoded value corresponding to `key` in the bencoded
+    dictionary `bs`."""
+    offset = 1
+    while offset < len(bs) and bs[offset] != ord("e"):
+        offset, curr_key = _str(bs, offset)
+        next_offset, _ = _parse(bs, offset)
         if curr_key == key:
-            return input[pos:next_pos]
-        pos = next_pos
+            return bs[offset:next_offset]
+        offset = next_offset
     raise KeyError
