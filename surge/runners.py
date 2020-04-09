601742cbf6a99069224bd499a4055cfb7f729482
zuqq <noahheld17@gmail.com>
2020-04-09 15:51:48 +0200

    Add signal handlers for silent exit

--- surge/runners.py
+++ surge/runners.py
@@ -1,20 +1,31 @@
 import asyncio
+import functools
 import logging
+import signal
+
+
+def handler(sig):
+    logging.debug("Received %r.", sig)
+    raise SystemExit(sig.value)
 
 
 def run(download):
-    # Adapted from aiohttp.
     loop = asyncio.get_event_loop()
-    asyncio.set_event_loop(loop)
     try:
-        loop.run_until_complete(download.start())
-        return loop.run_until_complete(download.wait_done())
-    except KeyboardInterrupt:
-        logging.debug("Received KeyboardInterrupt.")
-    finally:
-        loop.run_until_complete(download.stop())
-        tasks = [task for task in asyncio.all_tasks(loop) if not task.done()]
-        for task in tasks:
-            task.cancel()
-        loop.run_until_complete(asyncio.gather(*tasks, return_exceptions=True))
-        loop.run_until_complete(loop.shutdown_asyncgens())
+        for sig in (signal.SIGHUP, signal.SIGINT, signal.SIGTERM):
+            loop.add_signal_handler(sig, functools.partial(handler, sig))
+    except NotImplementedError:
+        # Windows doesn't have Unix signals.
+        pass
+
+    loop.run_until_complete(download.start())
+    rval = loop.run_until_complete(download.wait_done())
+
+    loop.run_until_complete(download.stop())
+    tasks = [task for task in asyncio.all_tasks(loop) if not task.done()]
+    for task in tasks:
+        task.cancel()
+    loop.run_until_complete(asyncio.gather(*tasks, return_exceptions=True))
+    loop.run_until_complete(loop.shutdown_asyncgens())
+
+    return rval
