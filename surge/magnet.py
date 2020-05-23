765ba08444ce43d0307ac2b0545a6c015b69a644
zuqq <noahheld17@gmail.com>
2020-05-23 23:21:20 +0200

    Parse magnet links more carefully

--- surge/magnet.py
+++ surge/magnet.py
@@ -7,9 +7,14 @@ def parse(magnet: str) -> Tuple[bytes, List[str]]:
     """Parse a magnet link into (info_hash, announce_list)."""
     url = urllib.parse.urlparse(magnet)
     qs = urllib.parse.parse_qs(url.query)
+
     (xt,) = qs["xt"]
-    # Strip "urn:btih:".
+    if not xt.startswith("urn:btih:"):
+        raise ValueError("Invalid value for 'xt'.")
     info_hash = bytes.fromhex(xt[9:])
+    if len(info_hash) != 20:
+        raise ValueError("Invalid info hash.")
+
     announce_list = qs.get("tr", [])
 
     return info_hash, announce_list
