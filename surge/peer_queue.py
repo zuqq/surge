76d44ac0f009a04c5f5673a46cac6c501d1fa915
zuqq <noahheld17@gmail.com>
2020-05-24 14:55:48 +0200

    Request peers from every tracker

--- surge/peer_queue.py
+++ surge/peer_queue.py
@@ -16,45 +16,53 @@ class PeerQueue(actor.Actor):
             self,
             announce_list: List[str],
             tracker_params: tracker.Parameters,
-            *,
-            max_tries: int = 5
         ):
         super().__init__()
 
         # Available trackers; unreachable or unresponsive trackers are discarded.
-        self._stack = announce_list[::-1]
+        self._announce_list = announce_list
         self._tracker_params = tracker_params
-        self._max_tries = max_tries
 
         self._sleep = None
         self._peers = asyncio.Queue()
+
+    async def _request_peers(self, announce, *, max_tries=5):
+        tries = 0
+        while tries < max_tries:
+            try:
+                resp = await tracker.request_peers(announce, self._tracker_params)
+            except Exception as e:
+                logging.warning("%r failed with %r", announce, e)
+                tries += 1
+            else:
+                logging.info("%r sent %r peers", announce, len(resp.peers))
+                return resp
+        logging.warning("%r unreachable", announce)
+        raise ConnectionError("Tracker unreachable.")
 
     ### Actor implementation
 
     async def _main_coro(self):
         seen_peers = set()
         while True:
-            if not self._stack:
-                raise RuntimeError("No trackers available.")
-            announce = self._stack[-1]
-            tries = 0
-            while tries < self._max_tries:
-                try:
-                    resp = await tracker.request_peers(announce, self._tracker_params)
-                except Exception as e:
-                    logging.warning("%r failed with %r", announce, e)
-                    tries += 1
-                else:
-                    logging.info("%r sent %r peers", announce, len(resp.peers))
-                    for peer in set(resp.peers) - seen_peers:
+            coros = []
+            for announce in self._announce_list:
+                coros.append(self._request_peers(announce))
+            results = await asyncio.gather(*coros, return_exceptions=True)
+
+            good_announces = []
+            interval = 3600
+            for result in results:
+                if not isinstance(result, ConnectionError):
+                    for peer in set(result.peers) - seen_peers:
                         self._peers.put_nowait(peer)
                         seen_peers.add(peer)
-                    self._sleep = asyncio.create_task(asyncio.sleep(resp.interval))
-                    await self._sleep
-                    break
-            else:
-                self._stack.pop()
-                logging.warning("%r dropped", announce)
+                    good_announces.append(result.announce)
+                    interval = max(interval, result.interval)
+            self._announce_list = good_announces
+
+            self._sleep = asyncio.create_task(asyncio.sleep(interval))
+            await self._sleep
 
     ### Queue interface
 
