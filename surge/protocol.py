bbed8877e11c24360f87a854241b8a36f9279cc9
zuqq <noahheld17@gmail.com>
2020-05-28 16:45:50 +0200

    Add a bunch of underscores

--- surge/protocol.py
+++ surge/protocol.py
@@ -12,7 +12,6 @@ class InsufficientData(Exception):
 
 class Protocol(asyncio.Protocol):
     def __init__(self, info_hash, peer_id, pieces):
-
         self._info_hash = info_hash
         self._peer_id = peer_id
         self._pieces = pieces
@@ -48,32 +47,32 @@ class Protocol(asyncio.Protocol):
             (Unchoked, Unchoked): self._block_data.put_nowait,
         }
 
-    ### State machine interface
+    ### State machine
 
     @property
-    def state(self):
+    def _state(self):
         return self.__class__
 
-    @state.setter
-    def state(self, next_state):
+    @_state.setter
+    def _state(self, next_state):
         self.__class__ = next_state
 
-    def transition(self, message_type, payload):
-        start_state = self.state
+    def _transition(self, message_type, payload):
+        start_state = self._state
         if (start_state, message_type) not in self._successor:
             return
         end_state = self._successor[(start_state, message_type)]
-        self.state = end_state
+        self._state = end_state
         if (start_state, end_state) in self._effect:
             self._effect[start_state, end_state](payload)
         if end_state.waiter is not None:
             end_state.waiter.set_result(None)
             end_state.waiter = None
 
-    def read_handshake(self):
+    def _read_handshake(self):
         if len(self._buffer) < 68:
             raise InsufficientData
-        self.state = Established
+        self._state = Established
         try:
             result = peer_protocol.parse_handshake(self._buffer[:68])
             self.handshake.set_result(result)
@@ -81,41 +80,49 @@ class Protocol(asyncio.Protocol):
             self.handshake.set_exception(e)
         del self._buffer[:68]
 
-    def read_messages(self):
+    def _read_messages(self):
         while len(self._buffer) >= 4:
             n = int.from_bytes(self._buffer[:4], "big")
             if len(self._buffer) < 4 + n:
                 break
             message = bytes(self._buffer[4 : 4 + n])
             del self._buffer[: 4 + n]
-            self.transition(*peer_protocol.parse(message, self._pieces))
+            self._transition(*peer_protocol.parse(message, self._pieces))
 
-    ### asyncio.Protocol interface
+    ### asyncio.Protocol implementation
 
     def connection_made(self, transport):
-        self.state = Open
+        self._state = Open
         self._transport = transport
-        self.write(peer_protocol.handshake(self._info_hash, self._peer_id))
+        self._write(peer_protocol.handshake(self._info_hash, self._peer_id))
 
     def connection_lost(self, exc):
         if self._exc is None:
             self._exc = exc
-        self.state = Protocol
+        self._state = Protocol
         self._closed.set()
 
     def data_received(self, data):
         self._buffer.extend(data)
-        self.read()
+        self._read()
 
     ### Stream interface
 
-    def read(self):
+    def _read(self):
         raise ConnectionError("Reading from closed connection.")
 
-    def write(self, data):
+    def _write(self, data):
         if self._exc is not None:
             raise self._exc
         self._transport.write(data)
+
+    ### Public interface
+
+    async def request(self, block: metadata.Block):
+        raise ConnectionError("Requesting on closed connection.")
+
+    async def receive(self) -> Tuple[metadata.Block, bytes]:
+        return await self._block_data.get()
 
     def close(self):
         if self._transport is not None:
@@ -125,29 +132,22 @@ class Protocol(asyncio.Protocol):
         # Don't raise here, because we're closing the connection anyway.
         await self._closed.wait()
 
-    ### Protocol interface
-
-    async def request(self, block: metadata.Block):
-        raise ConnectionError("Requesting on closed connection.")
-
-    async def receive(self) -> Tuple[metadata.Block, bytes]:
-        return await self._block_data.get()
-
 
 class Open(Protocol):
-    def read(self):
+    def _read(self):
         try:
-            self.read_handshake()
+            self._read_handshake()
         except InsufficientData:
             return
-        self.read_messages()
+        # There might be more messages in the buffer, so we should consume them.
+        self._read_messages()
 
 
 class Established(Protocol):
     waiter = None
 
-    def read(self):
-        self.read_messages()
+    def _read(self):
+        self._read_messages()
 
 
 class Choked(Established):
@@ -155,12 +155,12 @@ class Choked(Established):
         # Register with Unchoked so that we are woken up if the unchoke happens.
         loop = asyncio.get_running_loop()
         Unchoked.waiter = loop.create_future()
-        self.write(peer_protocol.interested())
+        self._write(peer_protocol.interested())
         # Wait for the unchoke to happen.
         await Unchoked.waiter
-        self.write(peer_protocol.request(block))
+        self._write(peer_protocol.request(block))
 
 
 class Unchoked(Established):
     async def request(self, block):
-        self.write(peer_protocol.request(block))
+        self._write(peer_protocol.request(block))
