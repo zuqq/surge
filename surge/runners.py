431d3216ea669fbe3e953f97865d36cd2d09799f
zuqq <noahheld17@gmail.com>
2020-06-02 14:21:18 +0200

    Slim down runners

--- surge/runners.py
+++ surge/runners.py
@@ -16,15 +16,7 @@ def run(actor):
             loop.add_signal_handler(sig, functools.partial(handler, sig))
     except NotImplementedError:
         pass
-
     loop.run_until_complete(actor.start())
     result = loop.run_until_complete(actor.result)
-
     loop.run_until_complete(actor.stop())
-    tasks = [task for task in asyncio.all_tasks(loop) if not task.done()]
-    for task in tasks:
-        task.cancel()
-    loop.run_until_complete(asyncio.gather(*tasks, return_exceptions=True))
-    loop.run_until_complete(loop.shutdown_asyncgens())
-
     return result
