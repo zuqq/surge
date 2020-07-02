2f78949b941239125192f45766970857677d019f
zuqq <noahheld17@gmail.com>
2020-07-02 19:10:52 +0200

    Switch to unittest

--- tests/test_bencoding.py
+++ tests/test_bencoding.py
@@ -1,46 +1,50 @@
-import pytest
+import unittest
 
 from surge import bencoding
 
 
-# Examples from BEP 3.
-valid = [
-    (b"i3e", 3),
-    (b"i-3e", -3),
-    (b"i0e", 0),
-    (b"l4:spam4:eggse", [b"spam", b"eggs"]),
-    (b"d3:cow3:moo4:spam4:eggse", {b"cow": b"moo", b"spam": b"eggs"}),
-    (b"d4:spaml1:a1:bee", {b"spam": [b"a", b"b"]}),
-]
-
-
-def test_decode():
-    for x, y in valid:
-        assert bencoding.decode(x) == y
-
-    for x in [b"", b"ie", b"iae", b"dde", b"2:abc"]:
-        with pytest.raises(ValueError):
-            bencoding.decode(x)
-
-
-def test_encode():
-    for x, y in valid:
-        assert bencoding.encode(y) == x
-
-    for y in [None, "utf-8", 1.0, (0, 1), {0: 1}]:
-        with pytest.raises(TypeError):
-            bencoding.encode(y)
-
-
-def test_raw_val():
-    examples = [
-        (b"d3:cow3:moo4:spam4:eggse", b"cow", b"3:moo"),
-        (b"d3:cow3:moo4:spam4:eggse", b"spam", b"4:eggs"),
-        (b"d4:spaml1:a1:bee", b"spam", b"l1:a1:be"),
+class BencodingTest(unittest.TestCase):
+    # Examples from BEP 3.
+    valid = [
+        (b"i3e", 3),
+        (b"i-3e", -3),
+        (b"i0e", 0),
+        (b"l4:spam4:eggse", [b"spam", b"eggs"]),
+        (b"d3:cow3:moo4:spam4:eggse", {b"cow": b"moo", b"spam": b"eggs"}),
+        (b"d4:spaml1:a1:bee", {b"spam": [b"a", b"b"]}),
     ]
 
-    for x, k, v in examples:
-        assert bencoding.raw_val(x, k) == v
+    def test_decode(self):
+        for x, y in self.valid:
+            with self.subTest(x=x, y=y):
+                self.assertEqual(bencoding.decode(x), y)
 
-    with pytest.raises(KeyError):
-        bencoding.raw_val(b"d4:spaml1:a1:bee", b"eggs")
+        for x in [b"", b"ie", b"iae", b"dde", b"2:abc"]:
+            with self.subTest(x=x):
+                with self.assertRaises(ValueError):
+                    bencoding.decode(x)
+
+    def test_encode(self):
+        for x, y in self.valid:
+            with self.subTest(x=x, y=y):
+                self.assertEqual(bencoding.encode(y), x)
+
+        for y in [None, "utf-8", 1.0, (0, 1), {0: 1}]:
+            with self.subTest(y=y):
+                with self.assertRaises(TypeError):
+                    bencoding.encode(y)
+
+    def test_raw_val(self):
+        examples = [
+            (b"d3:cow3:moo4:spam4:eggse", b"cow", b"3:moo"),
+            (b"d3:cow3:moo4:spam4:eggse", b"spam", b"4:eggs"),
+            (b"d4:spaml1:a1:bee", b"spam", b"l1:a1:be"),
+        ]
+
+        for x, k, v in examples:
+            with self.subTest(x=x, k=k, v=v):
+                self.assertEqual(bencoding.raw_val(x, k), v)
+
+        with self.subTest("KeyError"):
+            with self.assertRaises(KeyError):
+                bencoding.raw_val(b"d4:spaml1:a1:bee", b"eggs")
--- tests/test_magnet.py
+++ tests/test_magnet.py
@@ -1,24 +1,32 @@
-import pytest
+import unittest
 
 from surge import magnet
 
 
-def test_parse():
-    assert magnet.parse(
-        "magnet:?xt=urn:btih:86d4c80024a469be4c50bc5a102cf71780310074"
-        "&dn=debian-10.2.0-amd64-netinst.iso"
-        "&tr=http%3A%2F%2Fbttracker.debian.org%3A6969%2Fannounce"
-    ) == (
-        b"\x86\xd4\xc8\x00$\xa4i\xbeLP\xbcZ\x10,\xf7\x17\x801\x00t",
-        ["http://bttracker.debian.org:6969/announce"],
-    )
-    with pytest.raises(ValueError):
-        magnet.parse("http://www.example.com")
-    with pytest.raises(ValueError):
-        magnet.parse("magnet:?tr=http%3A%2F%2Fbttracker.debian.org%3A6969%2Fannounce")
-    with pytest.raises(ValueError):
-        magnet.parse("magnet:?xt=aaa")
-    with pytest.raises(ValueError):
-        magnet.parse("magnet:?xt=urn:btih:0")
-    with pytest.raises(ValueError):
-        magnet.parse("magnet:?xt=urn:btih:86d4")
+class ParseTest(unittest.TestCase):
+    def test_parse(self):
+        with self.subTest("valid"):
+            self.assertEqual(
+                magnet.parse(
+                    "magnet:?xt=urn:btih:86d4c80024a469be4c50bc5a102cf71780310074"
+                    "&dn=debian-10.2.0-amd64-netinst.iso"
+                    "&tr=http%3A%2F%2Fbttracker.debian.org%3A6969%2Fannounce"
+                ),
+                (
+                    b"\x86\xd4\xc8\x00$\xa4i\xbeLP\xbcZ\x10,\xf7\x17\x801\x00t",
+                    ["http://bttracker.debian.org:6969/announce"],
+                ),
+            )
+
+        invalid = [
+            "http://www.example.com",
+            "magnet:?tr=http%3A%2F%2Fbttracker.debian.org%3A6969%2Fannounce",
+            "magnet:?xt=aaa",
+            "magnet:?xt=urn:btih:0",
+            "magnet:?xt=urn:btih:86d4",
+        ]
+
+        for s in invalid:
+            with self.subTest(s=s):
+                with self.assertRaises(ValueError):
+                    magnet.parse(s)
