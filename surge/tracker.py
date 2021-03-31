ad764db4c7761e72dd3a7c175c495555c0fb3c42
zuqq <noahheld17@gmail.com>
2021-03-31 14:25:57 +0200

    Make the `tracker` package a module

--- surge/tracker.py
+++ surge/tracker.py
@@ -5,6 +5,8 @@ functions that implement the original tracker protocol over HTTP and its
 UDP-based variant.
 """
 
+from typing import ClassVar, List
+
 import asyncio
 import collections
 import contextlib
@@ -12,15 +14,66 @@ import dataclasses
 import functools
 import http.client
 import secrets
+import struct
 import time
 import urllib.parse
 
-from . import messages
-from .. import bencoding
-from ._metadata import Parameters, Peer, Result
+from . import bencoding
 
 
-__all__ = ("Parameters", "Peer", "request_peers_http", "request_peers_udp")
+@dataclasses.dataclass
+class Parameters:
+    info_hash: bytes
+    peer_id: bytes
+    port: int = 6881
+    uploaded: int = 0
+    downloaded: int = 0
+    # This is initialized to 0 because we also need to connect to the tracker
+    # before downloading the metadata.
+    left: int = 0
+    event: str = "started"
+    compact: int = 1  # See BEP 23.
+
+
+@dataclasses.dataclass(frozen=True)
+class Peer:
+    address: str
+    port: int
+
+    @classmethod
+    def from_bytes(cls, bs):
+        return cls(".".join(str(b) for b in bs[:4]), int.from_bytes(bs[4:], "big"))
+
+    @classmethod
+    def from_dict(cls, d):
+        return cls(d[b"ip"].decode(), d[b"port"])
+
+
+def _parse_peers(raw_peers):
+    peers = []
+    for i in range(0, len(raw_peers), 6):
+        peers.append(Peer.from_bytes(raw_peers[i : i + 6]))
+    return peers
+
+
+@dataclasses.dataclass
+class Result:
+    interval: int
+    peers: List[Peer]
+
+    @classmethod
+    def from_bytes(cls, interval, raw_peers):
+        return cls(interval, _parse_peers(raw_peers))
+
+    @classmethod
+    def from_dict(cls, resp):
+        if isinstance(resp[b"peers"], list):
+            # Dictionary model, as defined in BEP 3.
+            peers = [Peer.from_dict(d) for d in resp[b"peers"]]
+        else:
+            # Binary model ("compact format") from BEP 23.
+            peers = _parse_peers(resp[b"peers"])
+        return cls(resp[b"interval"], peers)
 
 
 def get(url, parameters):
@@ -71,7 +124,7 @@ class UDPTrackerProtocol(asyncio.DatagramProtocol):
             waiter = asyncio.get_running_loop().create_future()
             self._waiter = waiter
             await waiter
-        return messages.parse(self._queue.popleft())
+        return parse(self._queue.popleft())
 
     def write(self, message):
         # I'm omitting flow control because every write is followed by a read.
@@ -138,6 +191,74 @@ async def request_peers_http(root, url, parameters):
         root.remove_tracker(asyncio.current_task())
 
 
+@dataclasses.dataclass
+class ConnectRequest:
+    value: ClassVar[int] = 0
+    transaction_id: bytes
+
+    def to_bytes(self):
+        return struct.pack(">ql4s", 0x41727101980, self.value, self.transaction_id)
+
+
+@dataclasses.dataclass
+class AnnounceRequest:
+    value: ClassVar[int] = 1
+    transaction_id: bytes
+    connection_id: bytes
+    parameters: Parameters
+
+    def to_bytes(self) -> bytes:
+        return struct.pack(
+            ">8sl4s20s20sqqqlL4slH",
+            self.connection_id,
+            self.value,
+            self.transaction_id,
+            self.parameters.info_hash,
+            self.parameters.peer_id,
+            self.parameters.downloaded,
+            self.parameters.left,
+            self.parameters.uploaded,
+            0,
+            0,
+            secrets.token_bytes(4),
+            -1,
+            self.parameters.port,
+        )
+
+
+@dataclasses.dataclass
+class ConnectResponse:
+    value: ClassVar[int] = 0
+    connection_id: bytes
+
+    @classmethod
+    def from_bytes(cls, data):
+        _, _, connection_id = struct.unpack(">l4s8s", data)
+        return cls(connection_id)
+
+
+@dataclasses.dataclass
+class AnnounceResponse:
+    value: ClassVar[int] = 1
+    result: Result
+
+    @classmethod
+    def from_bytes(cls, data):
+        _, _, interval, _, _ = struct.unpack(">l4slll", data[:20])
+        return cls(Result.from_bytes(interval, data[20:]))
+
+
+def parse(data):
+    if len(data) < 4:
+        raise ValueError("Not enough bytes.")
+    value = int.from_bytes(data[:4], "big")
+    if value == ConnectResponse.value:
+        return ConnectResponse.from_bytes(data)
+    if value == AnnounceResponse.value:
+        return AnnounceResponse.from_bytes(data)
+    raise ValueError("Unkown message identifier.")
+
+
 async def request_peers_udp(root, url, parameters):
     try:
         while True:
@@ -147,26 +268,24 @@ async def request_peers_udp(root, url, parameters):
                     timeout = 15 * 2 ** n
                     if not connected:
                         transaction_id = secrets.token_bytes(4)
-                        protocol.write(messages.ConnectRequest(transaction_id))
+                        protocol.write(ConnectRequest(transaction_id))
                         try:
                             received = await asyncio.wait_for(protocol.read(), timeout)
                         except asyncio.TimeoutError:
                             continue
-                        if isinstance(received, messages.ConnectResponse):
+                        if isinstance(received, ConnectResponse):
                             connected = True
                             connection_id = received.connection_id
                             connection_time = time.monotonic()
                     if connected:
                         protocol.write(
-                            messages.AnnounceRequest(
-                                transaction_id, connection_id, parameters
-                            )
+                            AnnounceRequest(transaction_id, connection_id, parameters)
                         )
                         try:
                             received = await asyncio.wait_for(protocol.read(), timeout)
                         except asyncio.TimeoutError:
                             continue
-                        if isinstance(received, messages.AnnounceResponse):
+                        if isinstance(received, AnnounceResponse):
                             result = received.result
                             break
                         if time.monotonic() - connection_time >= 60:
