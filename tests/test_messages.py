1d13919232a35d5a402eb1522ab37961a06e93d5
zuqq <noahheld17@gmail.com>
2022-05-28 23:16:14 +0100

    Make `test_messages` less insane

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -1,149 +1,71 @@
-import hashlib
-import struct
 import unittest
 
-from surge import _metadata
-from surge import bencoding
 from surge import messages
 
 
-def _extension_message(extension_value, payload):
-    n = len(payload)
-    return struct.pack(f">LBB{n}s", n + 2, 20, extension_value, payload)
-
-
 class TestMessages(unittest.TestCase):
-    piece_data = b"a\n"
-    n = len(piece_data)
-    piece = _metadata.Piece(0, 0, n, hashlib.sha1(piece_data).digest())
-    raw_info = bencoding.encode(
-        {
-            b"length": n,
-            b"name": b"a",
-            b"piece_length": 2 ** 18,
-            b"pieces": piece.hash,
-        }
+    handshake_reference = b"\x13BitTorrent protocol\x00\x00\x00\x00\x00\x10\x00\x00\xc8\x06\x12\xfd(\xfb\xfa3g\xf2\xe9\x0c5\xd9\x9f\x93\xa1\xd24O\xad6n\x84\xb3a\xa4\xc1\xa1\xde\xd4H\x01J\xc0]\x1b\x88\x92I"
+    handshake = messages.Handshake(
+        messages.EXTENSION_PROTOCOL_BIT,
+        b"\xc8\x06\x12\xfd(\xfb\xfa3g\xf2\xe9\x0c5\xd9\x9f\x93\xa1\xd24O",
+        b"\xad6n\x84\xb3a\xa4\xc1\xa1\xde\xd4H\x01J\xc0]\x1b\x88\x92I",
     )
-
-    reserved = 1 << 20
-    info_hash = hashlib.sha1(raw_info).digest()
-    peer_id = b"\xad6n\x84\xb3a\xa4\xc1\xa1\xde\xd4H\x01J\xc0]\x1b\x88\x92I"
-    handshake_reference = struct.pack(
-        ">B19sQ20s20s", 19, b"BitTorrent protocol", reserved, info_hash, peer_id
-    )
-    handshake = messages.Handshake(reserved, info_hash, peer_id)
-
-    valid = [
-        (struct.pack(">L", 0), messages.Keepalive()),
-        (struct.pack(">LB", 1, 0), messages.Choke()),
-        (struct.pack(">LB", 1, 1), messages.Unchoke()),
-        (struct.pack(">LB", 1, 2), messages.Interested()),
-        (struct.pack(">LB", 1, 3), messages.NotInterested()),
-        (struct.pack(">LBL", 5, 4, piece.index), messages.Have(piece.index)),
-    ]
-
-    piece_indices = {0}
-    bitfield = messages.Bitfield.from_indices(piece_indices, len(piece_indices))
-    valid.append((struct.pack(">LBB", 2, 5, 1 << 7), bitfield))
-
-    block = _metadata.Block(piece, 0, n)
-
-    valid.append(
+    bitfield_reference = b"\x00\x00\x00\x02\x05\x80"
+    bitfield = messages.Bitfield.from_indices({0}, 1)
+    valid = (
+        (b"\x00\x00\x00\x00", messages.Keepalive()),
+        (b"\x00\x00\x00\x01\x00", messages.Choke()),
+        (b"\x00\x00\x00\x01\x01", messages.Unchoke()),
+        (b"\x00\x00\x00\x01\x02", messages.Interested()),
+        (b"\x00\x00\x00\x01\x03", messages.NotInterested()),
+        (b"\x00\x00\x00\x05\x04\x00\x00\x00\x00", messages.Have(0)),
         (
-            struct.pack(">LBLLL", 13, 6, block.piece.index, block.begin, block.length),
-            messages.Request.from_block(block),
-        )
-    )
-
-    valid.append(
+            b"\x00\x00\x00\r\x06\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02",
+            messages.Request(0, 0, 2),
+        ),
         (
-            struct.pack(
-                f">LBLL{n}s", n + 9, 7, block.piece.index, block.begin, piece_data
-            ),
-            messages.Block.from_block(block, piece_data),
-        )
-    )
-
-    valid.append(
+            b"\x00\x00\x00\x0b\x07\x00\x00\x00\x00\x00\x00\x00\x00a\n",
+            messages.Block(0, 0, b"a\n"),
+        ),
         (
-            struct.pack(">LBLLL", 13, 8, block.piece.index, block.begin, block.length),
-            messages.Cancel.from_block(block),
-        )
-    )
-
-    ut_metadata = 3
-    metadata_size = len(raw_info)
-
-    valid.append(
+            b"\x00\x00\x00\r\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02",
+            messages.Cancel(0, 0, 2),
+        ),
         (
-            _extension_message(
-                0,
-                bencoding.encode(
-                    {
-                        b"m": {b"ut_metadata": ut_metadata},
-                        b"metadata_size": metadata_size,
-                    }
-                ),
-            ),
-            messages.ExtensionHandshake(ut_metadata, metadata_size),
-        )
-    )
-
-    metadata_index = 0
-
-    valid.append(
+            b"\x00\x00\x00.\x14\x00d1:md11:ut_metadatai3ee13:metadata_sizei76ee",
+            messages.ExtensionHandshake(3, 76),
+        ),
         (
-            _extension_message(
-                ut_metadata,
-                b"".join(
-                    (
-                        bencoding.encode(
-                            {
-                                b"msg_type": 1,
-                                b"piece": metadata_index,
-                                b"total_size": metadata_size,
-                            }
-                        ),
-                        raw_info,
-                    )
-                ),
-            ),
+            b"\x00\x00\x00x\x14\x03d8:msg_typei1e5:piecei0e10:total_sizei76eed6:lengthi2e4:name1:a12:piece_lengthi262144e6:pieces20:?xhP\xe3\x87U\x0f\xda\xb86\xed~m\xc8\x81\xde#\x00\x1be",
             messages.MetadataData(
-                metadata_index, metadata_size, raw_info, ut_metadata=ut_metadata
+                0,
+                76,
+                b"d6:lengthi2e4:name1:a12:piece_lengthi262144e6:pieces20:?xhP\xe3\x87U\x0f\xda\xb86\xed~m\xc8\x81\xde#\x00\x1be",
             ),
-        )
-    )
-
-    valid.append(
+        ),
         (
-            _extension_message(
-                ut_metadata,
-                bencoding.encode({b"msg_type": 2, b"piece": metadata_index}),
-            ),
-            messages.MetadataReject(metadata_index, ut_metadata=ut_metadata),
-        )
-    )
-
-    valid.append(
+            b"\x00\x00\x00\x1b\x14\x03d8:msg_typei2e5:piecei0ee",
+            messages.MetadataReject(0),
+        ),
         (
-            _extension_message(
-                ut_metadata,
-                bencoding.encode({b"msg_type": 0, b"piece": metadata_index}),
-            ),
-            messages.MetadataRequest(metadata_index, ut_metadata=ut_metadata),
-        )
+            b"\x00\x00\x00\x1b\x14\x03d8:msg_typei0e5:piecei0ee",
+            messages.MetadataRequest(0),
+        ),
     )
 
     def test_to_bytes(self):
         with self.subTest(self.handshake):
             self.assertEqual(self.handshake.to_bytes(), self.handshake_reference)
 
+        with self.subTest(self.bitfield):
+            self.assertEqual(self.bitfield.to_bytes(), self.bitfield_reference)
+
         for x, y in self.valid:
             with self.subTest(y):
                 self.assertEqual(y.to_bytes(), x)
 
     def test_to_indices(self):
-        self.assertEqual(self.bitfield.to_indices(), self.piece_indices)
+        self.assertEqual(self.bitfield.to_indices(), {0})
 
     def test_parse_handshake(self):
         self.assertEqual(
@@ -151,54 +73,41 @@ class TestMessages(unittest.TestCase):
         )
 
     def test_parse(self):
+        with self.subTest(self.bitfield_reference):
+            self.assertEqual(messages.parse(self.bitfield_reference), self.bitfield)
+
         for x, y in self.valid:
             with self.subTest(x):
                 self.assertEqual(messages.parse(x), y)
 
         with self.subTest("Missing prefix."):
             with self.assertRaises(ValueError):
-                messages.parse(struct.pack(">B", 0))
+                messages.parse(b"\x00")
 
         with self.subTest("Wrong prefix."):
             with self.assertRaises(ValueError):
-                messages.parse(struct.pack(">LB", 0, 0))
+                messages.parse(b"\x00\x00\x00\x00\x00")
 
         with self.subTest("Unknown value."):
             with self.assertRaises(ValueError):
-                messages.parse(struct.pack(">LB", 1, 21))
+                messages.parse(b"\x00\x00\x00\x01\x15")
 
         with self.subTest("Invalid extension protocol."):
             with self.assertRaises(ValueError):
-                messages.parse(struct.pack(">LBB", 2, 20, 1))
+                messages.parse(b"\x00\x00\x00\x02\x14\x01")
 
         with self.subTest("Missing b'msg_type'."):
             with self.assertRaises(ValueError):
-                messages.parse(
-                    _extension_message(self.ut_metadata, bencoding.encode({}))
-                )
+                messages.parse(b"\x00\x00\x00\x04\x14\x03de")
 
         with self.subTest("Missing b'piece'."):
             with self.assertRaises(ValueError):
-                messages.parse(
-                    _extension_message(
-                        self.ut_metadata, bencoding.encode({b"msg_type": 0})
-                    )
-                )
+                messages.parse(b"\x00\x00\x00\x11\x14\x03d8:msg_typei0ee")
 
         with self.subTest("Missing b'total_size'."):
             with self.assertRaises(ValueError):
-                messages.parse(
-                    _extension_message(
-                        self.ut_metadata,
-                        bencoding.encode({b"msg_type": 1, b"piece": 0}),
-                    )
-                )
+                messages.parse(b"\x00\x00\x00\x1b\x14\x03d8:msg_typei1e5:piecei0ee")
 
         with self.subTest("Invalid b'msg_type'."):
             with self.assertRaises(ValueError):
-                messages.parse(
-                    _extension_message(
-                        self.ut_metadata,
-                        bencoding.encode({b"msg_type": 3, b"piece": 0}),
-                    )
-                )
+                messages.parse(b"\x00\x00\x00\x1b\x14\x03d8:msg_typei3e5:piecei0ee")
