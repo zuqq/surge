17b739b076ed14d735ba7883d487a5592e03a6d5
zuqq <noahheld17@gmail.com>
2022-05-29 12:12:00 +0100

    Populate `store` at the top level

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -12,16 +12,9 @@ from surge.tracker import Trackers
 from . import _tracker
 
 
-async def upload(uploader_started, metadata, folder):
+async def upload(uploader_started, metadata, store):
     pieces = metadata.pieces
     info_hash = metadata.info_hash
-    chunks = _metadata.make_chunks(pieces, metadata.files)
-    store = []
-    for piece in pieces:
-        data = []
-        for chunk in chunks[piece]:
-            data.append(chunk.read(folder))
-        store.append(b"".join(data))
 
     async def _main(reader, writer):
         stream = Stream(reader, writer)
@@ -63,23 +56,30 @@ class TestProtocol(unittest.TestCase):
         with (folder / "example.torrent").open("rb") as f:
             raw_metadata = f.read()
         metadata = _metadata.Metadata.from_bytes(raw_metadata)
+        pieces = metadata.pieces
+        chunks = _metadata.make_chunks(pieces, metadata.files)
+        store = []
+        for piece in pieces:
+            data = []
+            for chunk in chunks[piece]:
+                data.append(chunk.read(folder))
+            store.append(b"".join(data))
 
         async def _main():
             tracker_started = asyncio.Event()
             uploader_started = asyncio.Event()
             tasks = {
                 asyncio.create_task(_tracker.serve_peers_http(tracker_started)),
-                asyncio.create_task(upload(uploader_started, metadata, folder)),
+                asyncio.create_task(upload(uploader_started, metadata, store)),
             }
             await asyncio.gather(tracker_started.wait(), uploader_started.wait())
             info_hash = metadata.info_hash
             peer_id = b"\xad6n\x84\xb3a\xa4\xc1\xa1\xde\xd4H\x01J\xc0]\x1b\x88\x92I"
             async with Trackers(info_hash, peer_id, metadata.announce_list, 50) as trackers:
-                pieces = metadata.pieces
-                missing_pieces = set(metadata.pieces)
+                missing_pieces = set(pieces)
                 max_peers = 50
                 results = Channel(max_peers)
-                torrent = protocol.Torrent(metadata.pieces, missing_pieces, results)
+                torrent = protocol.Torrent(pieces, missing_pieces, results)
                 for _ in range(max_peers):
                     tasks.add(
                         asyncio.create_task(
