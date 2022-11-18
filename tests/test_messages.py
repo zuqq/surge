d1a009cc2b5528e1e8d9931d10723e6b8fd67093
zuqq <noahheld17@gmail.com>
2022-11-18 20:09:05 +0000

    Make `test_messages` easier to read

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -4,21 +4,38 @@ from surge import messages
 
 
 class TestMessages(unittest.TestCase):
-    handshake_reference = b"\x13BitTorrent protocol\x00\x00\x00\x00\x00\x10\x00\x00\xc8\x06\x12\xfd(\xfb\xfa3g\xf2\xe9\x0c5\xd9\x9f\x93\xa1\xd24O\xad6n\x84\xb3a\xa4\xc1\xa1\xde\xd4H\x01J\xc0]\x1b\x88\x92I"
-    handshake = messages.Handshake(
-        messages.EXTENSION_PROTOCOL_BIT,
-        b"\xc8\x06\x12\xfd(\xfb\xfa3g\xf2\xe9\x0c5\xd9\x9f\x93\xa1\xd24O",
-        b"\xad6n\x84\xb3a\xa4\xc1\xa1\xde\xd4H\x01J\xc0]\x1b\x88\x92I",
-    )
+    info_hash = b"\xc8\x06\x12\xfd(\xfb\xfa3g\xf2\xe9\x0c5\xd9\x9f\x93\xa1\xd24O"
+    peer_id = b"\xad6n\x84\xb3a\xa4\xc1\xa1\xde\xd4H\x01J\xc0]\x1b\x88\x92I"
+    handshake_reference = b"\x13BitTorrent protocol\x00\x00\x00\x00\x00\x10\x00\x00" + info_hash + peer_id
+    handshake = messages.Handshake(messages.EXTENSION_PROTOCOL_BIT, info_hash, peer_id)
     bitfield_reference = b"\x00\x00\x00\x02\x05\x80"
     bitfield = messages.Bitfield.from_indices({0}, 1)
+    metadata = b"d6:lengthi2e4:name1:a12:piece_lengthi262144e6:pieces20:?xhP\xe3\x87U\x0f\xda\xb86\xed~m\xc8\x81\xde#\x00\x1be"
     valid = [
-        (b"\x00\x00\x00\x00", messages.Keepalive()),
-        (b"\x00\x00\x00\x01\x00", messages.Choke()),
-        (b"\x00\x00\x00\x01\x01", messages.Unchoke()),
-        (b"\x00\x00\x00\x01\x02", messages.Interested()),
-        (b"\x00\x00\x00\x01\x03", messages.NotInterested()),
-        (b"\x00\x00\x00\x05\x04\x00\x00\x00\x00", messages.Have(0)),
+        (
+            b"\x00\x00\x00\x00",
+            messages.Keepalive(),
+        ),
+        (
+            b"\x00\x00\x00\x01\x00",
+            messages.Choke(),
+        ),
+        (
+            b"\x00\x00\x00\x01\x01",
+            messages.Unchoke(),
+        ),
+        (
+            b"\x00\x00\x00\x01\x02",
+            messages.Interested(),
+        ),
+        (
+            b"\x00\x00\x00\x01\x03",
+            messages.NotInterested(),
+        ),
+        (
+            b"\x00\x00\x00\x05\x04\x00\x00\x00\x00",
+            messages.Have(0),
+        ),
         (
             b"\x00\x00\x00\r\x06\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02",
             messages.Request(0, 0, 2),
@@ -36,12 +53,8 @@ class TestMessages(unittest.TestCase):
             messages.ExtensionHandshake(3, 76),
         ),
         (
-            b"\x00\x00\x00x\x14\x03d8:msg_typei1e5:piecei0e10:total_sizei76eed6:lengthi2e4:name1:a12:piece_lengthi262144e6:pieces20:?xhP\xe3\x87U\x0f\xda\xb86\xed~m\xc8\x81\xde#\x00\x1be",
-            messages.MetadataData(
-                0,
-                76,
-                b"d6:lengthi2e4:name1:a12:piece_lengthi262144e6:pieces20:?xhP\xe3\x87U\x0f\xda\xb86\xed~m\xc8\x81\xde#\x00\x1be",
-            ),
+            b"\x00\x00\x00x\x14\x03d8:msg_typei1e5:piecei0e10:total_sizei76ee" + metadata,
+            messages.MetadataData(0, 76, metadata),
         ),
         (
             b"\x00\x00\x00\x1b\x14\x03d8:msg_typei2e5:piecei0ee",
@@ -68,9 +81,7 @@ class TestMessages(unittest.TestCase):
         self.assertEqual(self.bitfield.to_indices(), {0})
 
     def test_parse_handshake(self):
-        self.assertEqual(
-            messages.parse_handshake(self.handshake_reference), self.handshake
-        )
+        self.assertEqual(messages.parse_handshake(self.handshake_reference), self.handshake)
 
     def test_parse(self):
         with self.subTest(self.bitfield_reference):
